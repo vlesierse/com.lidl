7364fe085d8455125f70e0f27153115e5d9b7d4d
Vincent Lesierse <vincent@lesierse.com>
2021-03-24 09:38:15 +0100

    Simplify the RGB LED Strip

--- drivers/rgb_led_strip/device.js
+++ drivers/rgb_led_strip/device.js
@@ -1,7 +1,13 @@
 'use strict';
 
-const TuyaZigBeeLightDevice = require('../../lib/TuyaZigBeeLightDevice');
+//const TuyaZigBeeLightDevice = require('../../lib/TuyaZigBeeLightDevice');
 
-class rgb_led_strip extends TuyaZigBeeLightDevice {}
+//class rgb_led_strip extends TuyaZigBeeLightDevice {}
 
-module.exports = rgb_led_strip;
+//module.exports = rgb_led_strip;
+
+const { ZigBeeLightDevice } = require("homey-zigbeedriver");
+
+class RGBLedStrip extends ZigBeeLightDevice { }
+
+module.exports = RGBLedStrip;
